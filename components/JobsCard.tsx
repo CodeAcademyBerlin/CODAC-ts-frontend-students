a44054760cb0d681062fd6bc1794a4f268cd455e
BetoCarrillo <alberto.carrillo01@gmail.com>
2022-11-14 23:10:59 +0100

    filter buttons ready

--- components/JobsCard.tsx
+++ components/JobsCard.tsx
@@ -1,89 +1,154 @@
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import Button from '@mui/material/Button'
-import Divider from '@mui/material/Divider'
-import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
-import Collapse from '@mui/material/Collapse'
-import IconButton from '@mui/material/IconButton'
-import CardActions from '@mui/material/CardActions'
-import ChevronUp from 'mdi-material-ui/ChevronUp'
-import ChevronDown from 'mdi-material-ui/ChevronDown'
-import React, { useState } from 'react'
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import CardActions from "@mui/material/CardActions";
+import ChevronUp from "mdi-material-ui/ChevronUp";
+import ChevronDown from "mdi-material-ui/ChevronDown";
+import React, { useState } from "react";
+import { JobPost } from "../graphql/_generated_";
+import Link from "next/link";
+import { useTheme } from "@mui/system";
 
-import { JobPost } from '../graphql/_generated_'
+type Href = Object;
 
-
-
-function JobsCard({ job }: {job: JobPost}) {  
-    console.log(job)
-      const [collapse, setCollapse] = useState<boolean>(false)
-    const handleClick = () => {
-    setCollapse((current) => !current)
-  }  
+function JobsCard({ job }: { job: JobPost }) {
+  const theme = useTheme();
+  const [collapse, setCollapse] = useState<boolean>(false);
+  const handleClick = () => {
+    setCollapse((current) => !current);
+  };
   return (
-    <div> <Card sx={{ position: 'relative',  paddingBottom: '2px', }}>
-          <CardContent sx={{
-              paddingBottom: '0px',
-          }}>
-        <Box
+    <div>
+      {" "}
+      <Card
+        sx={{
+          position: "relative",
+          borderRadius: theme.shape.borderRadius,
+          paddingBottom: "2px",
+        }}
+      >
+        <CardContent
           sx={{
-
-            mb: 5,
-            display: 'flex',
-            flexWrap: 'wrap',
-            alignItems: 'center',
-            justifyContent: 'space-between'
+            paddingBottom: "0px",
           }}
         >
-          <Box sx={{    display: 'flex', flexDirection: 'column'}}>
-            <Typography variant='h6'>{job.position}</Typography>
-            <Typography sx={{ fontWeight: 600, fontSize: '1rem' }} variant='caption'>{job.company}</Typography>
+          <Box
+            sx={{
+              mb: 5,
+              display: "flex",
+              flexWrap: "wrap",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography variant="h6">{job.position}</Typography>
+              <Typography
+                sx={{ fontWeight: 600, fontSize: "1rem" }}
+                variant="caption"
+              >
+                {job.company}
+              </Typography>
+            </Box>
+            <Button sx={{ color: theme.palette.mode }} variant="contained">
+              {" "}
+              <Link
+                href={job.url}
+                target="_blank"
+                rel="noopener"
+                className="noDeco"
+              >
+                Apply
+              </Link>
+            </Button>
           </Box>
-          <Button variant='contained'>Apply now</Button>
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection:'column', flexWrap: 'wrap', justifyContent: 'flex-start', alignItems: 'start', }}>
-                  <Typography variant='subtitle2' sx={{ whiteSpace: 'nowrap', color: 'text.primary', fontSize: '0.8rem' }}> <span className='boldText'>Field:</span>  {job.fileld}
-                                        
-
-          
-                  </Typography>
-                  <Typography variant='subtitle2' sx={{ whiteSpace: 'nowrap', color: 'text.primary', fontSize: '0.8rem' }}> <span className='boldText' >Date:</span>&nbsp;{new Date(job.updatedAt).toLocaleString("de-DE", {
-                                day: "2-digit",
-                                month: "2-digit",
-                                year: "2-digit",
-                              })}
-
-          
-          </Typography>    
-        </Box>
-                              </CardContent>
-                               <CardActions className='card-action-dense'>
-        <Box
-                                      sx={{
-             
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}
-        >
-                                      <Button onClick={(e) => { handleClick() }}>More</Button>
-          <IconButton size='small' onClick={(e) => { handleClick() }}>
-            {collapse ? <ChevronUp sx={{ fontSize: '1.875rem' }} /> : <ChevronDown sx={{  fontSize: '1.875rem' }} />}
-          </IconButton>
-        </Box>
-      </CardActions>
-      <Collapse in={collapse}>
-        <Divider sx={{ margin: 0 }} />
-        <CardContent>
-          <Typography variant='body2'>
-          {job.description}
-          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              flexWrap: "wrap",
+              justifyContent: "flex-start",
+              alignItems: "start",
+            }}
+          >
+            <Typography
+              variant="subtitle2"
+              sx={{
+                whiteSpace: "nowrap",
+                color: "text.primary",
+                fontSize: "0.8rem",
+              }}
+            >
+              {" "}
+              <span className="boldText">Field:</span>{" "}
+              {job.fileld?.replace("_", " ")}
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              sx={{
+                whiteSpace: "nowrap",
+                color: "text.primary",
+                fontSize: "0.8rem",
+              }}
+            >
+              {" "}
+              <span className="boldText">Date:</span>&nbsp;
+              {new Date(job.updatedAt).toLocaleString("de-DE", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "2-digit",
+              })}
+            </Typography>
+          </Box>
         </CardContent>
-      </Collapse>
-    </Card></div>
-  )
+        <CardActions className="card-action-dense">
+          {job.description === "" ? (
+            ""
+          ) : (
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Button
+                onClick={(e) => {
+                  handleClick();
+                }}
+              >
+                More
+              </Button>
+              <IconButton
+                size="small"
+                onClick={(e) => {
+                  handleClick();
+                }}
+              >
+                {collapse ? (
+                  <ChevronUp sx={{ fontSize: "1.875rem" }} />
+                ) : (
+                  <ChevronDown sx={{ fontSize: "1.875rem" }} />
+                )}
+              </IconButton>
+            </Box>
+          )}
+        </CardActions>
+        <Collapse in={collapse}>
+          <Divider sx={{ margin: 0 }} />
+          <CardContent>
+            <Typography variant="body2">{job.description}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  );
 }
 
-export default JobsCard
+export default JobsCard;
--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,89 +1,150 @@
+import client from "../configs/apollo-client";
+import { ApolloClient, ApolloLink, useQuery } from "@apollo/client";
+import { GET_JOBS } from "../graphql/queries";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Grid from "@mui/material/Grid";
+import { useState } from "react";
+import JobsCard from "../components/JobsCard";
+import { JobPost, JobPostEntity } from "../graphql/_generated_";
+import * as React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
-import client from '../configs/apollo-client'
-import { ApolloClient, ApolloLink, useQuery } from '@apollo/client';
-import { GET_JOBS } from '../graphql/queries';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import Grid from '@mui/material/Grid'
-import { useState } from 'react';
-import JobsCard from '../components/JobsCard';
-import { JobPost, JobPostEntity } from '../graphql/_generated_';
-   // ** types
+// ** types
 
-interface Data {data: Object
+interface Data {
+  data?: Object;
+  push?: any;
 }
 
-type index = number
-export type Event = Object
+type index = number;
 
+// export interface Event {
+//   e: Object;
+// }
 
-const jobs = ({ result }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-      // ** State
-  const [collapse, setCollapse] = useState<boolean>(false)
-    const handleClick = () => {
-    setCollapse((current) => !current)
-  }  
-const jobs = result.data.jobPosts.data
-console.log("jobs",jobs)
+const jobs = ({
+  result,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  // ** State
+  const [data, setData] = useState(result);
+
+  const handleClick = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    const value = e.target.value;
+    const result: Data = [];
+    jobs.map((jobEntity: JobPostEntity, i: index) => {
+      if (jobEntity.attributes?.fileld?.includes(value) === true) {
+        result.push(jobEntity);
+      }
+    });
+    setData(result);
+    console.log(data);
+    return data;
+    // const filtered = jobs.includes(e.target.value);
+    // console.log(filtered);
+  };
+
+  console.log(data);
+  const jobs = result.data.jobPosts.data;
+
   return (
+    <div>
       <div>
-               <Grid container spacing={6}>
-          {result &&
-                  jobs.map((jobEntity: JobPostEntity, i: index) => (           
-                      <Grid item xs={12} sm={6} md={4} key={i}>
-                        { jobEntity.attributes &&  <JobsCard job={jobEntity.attributes} />   }
-                          
-    
-          </Grid>
+        <FormControl>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+          >
+            <FormControlLabel
+              onClick={(e) => {
+                handleClick(e);
+              }}
+              value="All"
+              control={<Radio />}
+              label="All"
+            />
+            <FormControlLabel
+              onClick={(e) => {
+                handleClick(e);
+              }}
+              value="Web_Development"
+              control={<Radio />}
+              label="Web Development"
+            />
+            <FormControlLabel
+              onClick={(e) => {
+                handleClick(e);
+              }}
+              value="Data_Science"
+              control={<Radio />}
+              label="Data Science"
+            />
+            <FormControlLabel
+              onClick={(e) => {
+                handleClick(e);
+              }}
+              value="Other"
+              control={<Radio />}
+              label="Other"
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>{" "}
+      <br />
+      <Grid container spacing={6}>
+        {data &&
+          result.data.jobPosts.data.map(
+            (jobEntity: JobPostEntity, i: index) => (
+              <Grid item xs={12} sm={6} md={4} key={i}>
+                {jobEntity.attributes && (
+                  <JobsCard job={jobEntity.attributes} />
+                )}
+              </Grid>
+            )
+          )}
+      </Grid>
+    </div>
+  );
+};
 
-        ))} 
-          </Grid>
-      </div>
-  )
-}
-
-export default jobs
-
+export default jobs;
 
 // export async function getServerSideProps() {
 //     const data = await client.query({
 //         query: GET_JOBS
 //     });
 //     return {
-//       props: { data },   
+//       props: { data },
 //     }
 //     console.log("data", data)
 // }
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-export const getServerSideProps : GetServerSideProps = async (context) => {
-let myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-let graphql = JSON.stringify({
-  query: "query getJobs {\n    jobPosts{\n        data{\n            attributes{\n                position\n                company\n                fileld\n                createdAt\n                updatedAt\n                description  \n            }\n        }\n    }\n  }",
-  variables: {}
-})
-let requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: graphql,
+  let graphql = JSON.stringify({
+    query:
+      "query filterJobs ($filter: JobPostFiltersInput) {\n    jobPosts (filters: $filter) {\n        data {\n            attributes{\n                url\n                position\n                company\n                fileld \n                createdAt\n                updatedAt\n                description \n                \n            }\n        }\n    }\n  }",
+    //   variables: {"filter":{"fileld":{"eq":`IT`}}}
+  });
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: graphql,
+  };
+  const response = await fetch(
+    "https://codac-364707.ey.r.appspot.com/graphql",
+    requestOptions
+  );
+  const result: Data = await response.json();
+  return {
+    props: { result },
+  };
 };
-const response = await fetch("https://codac-364707.ey.r.appspot.com/graphql", requestOptions)
-const result: Data = await response.json()
-    console.log("RESULTS", result)
-    
-    return {
-      props: {result},   
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
