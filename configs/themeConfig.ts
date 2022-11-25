56a6cf71dc626f6ee8e6572d25622285cef8987d
Emily <emilyrachelstickler@gmail.com>
2022-11-25 17:19:23 +0100

    changes for build

--- configs/themeConfig.ts
+++ configs/themeConfig.ts
@@ -8,47 +8,47 @@ import { ContentWidth } from '../@core/layouts/types'
 import { ReactNode } from 'react'
 
 type ThemeConfig = {
-  // mode: PaletteMode
+  mode: PaletteMode
   templateName: string
-  // routingLoader: boolean
-  // disableRipple: boolean
-  // navigationSize: number
-  // menuTextTruncate: boolean
-  // contentWidth: ContentWidth
-  // responsiveFontSizes: boolean
+  routingLoader: boolean
+  disableRipple: boolean
+  navigationSize: number
+  menuTextTruncate: boolean
+  contentWidth: ContentWidth
+  responsiveFontSizes: boolean
 }
 
 const themeConfig: ThemeConfig = {
   // ** Layout Configs
   templateName: 'CODAC' /* App Name */,
-//   mode: 'light' /* light | dark */,
-//   contentWidth: 'boxed' /* full | boxed */,
+  mode: 'light' /* light | dark */,
+  contentWidth: 'boxed' /* full | boxed */,
 
-//   // ** Routing Configs
-//   routingLoader: true /* true | false */,
+  // ** Routing Configs
+  routingLoader: true /* true | false */,
 
-//   // ** Navigation (Menu) Configs
-//   menuTextTruncate: true /* true | false */,
-//   navigationSize: 260 /* Number in PX(Pixels) /*! Note: This is for Vertical navigation menu only */,
+  // ** Navigation (Menu) Configs
+  menuTextTruncate: true /* true | false */,
+  navigationSize: 260 /* Number in PX(Pixels) /*! Note: This is for Vertical navigation menu only */,
 
-//   // ** Other Configs
-//   responsiveFontSizes: true /* true | false */,
-//   disableRipple: false /* true | false */
+  // ** Other Configs
+  responsiveFontSizes: true /* true | false */,
+  disableRipple: false /* true | false */
 }
 
-export const lightTheme = {
-  body: '#FFF',
-  text: '#363537',
-  toggleBorder: '#FFF',
-  background: '#363537'
-}
+// export const lightTheme = {
+//   body: '#FFF',
+//   text: '#363537',
+//   toggleBorder: '#FFF',
+//   background: '#363537'
+// }
 
-export const darkTheme = {
-  body: '#363537',
-  text: '#FAFAFA',
-  toggleBorder: '#6B8096',
-  background: '#999',
-}
+// export const darkTheme = {
+//   body: '#363537',
+//   text: '#FAFAFA',
+//   toggleBorder: '#6B8096',
+//   background: '#999',
+// }
 
 //?what is DefaultTheme and how do I include my own properties on it?
 // export const GlobalStyles = createGlobalStyle`
