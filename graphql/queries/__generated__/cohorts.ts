99cecb1728afda439850d3a5dd4c334660eedb50
Emily <emilyrachelstickler@gmail.com>
2022-12-06 17:40:47 +0100

    cohorts query

---
+++ graphql/queries/__generated__/cohorts.ts
@@ -0,0 +1,91 @@
+import * as Types from '../../global/ __generated__/types';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+
+export const GetCohortsDocument = gql`
+    query getCohorts {
+  cohorts {
+    data {
+      attributes {
+        name
+        start_date
+        students {
+          data {
+            attributes {
+              firstname
+              lastname
+              start_date
+              end_date
+              main_course {
+                data {
+                  attributes {
+                    name
+                    achievements {
+                      data {
+                        attributes {
+                          name
+                          badge {
+                            data {
+                              attributes {
+                                name
+                                alternativeText
+                                caption
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              avatar {
+                data {
+                  attributes {
+                    alternativeText
+                    name
+                    caption
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetCohortsQuery__
+ *
+ * To run a query within a React component, call `useGetCohortsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetCohortsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetCohortsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetCohortsQuery(baseOptions?: Apollo.QueryHookOptions<GetCohortsQuery, GetCohortsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetCohortsQuery, GetCohortsQueryVariables>(GetCohortsDocument, options);
+      }
+export function useGetCohortsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetCohortsQuery, GetCohortsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetCohortsQuery, GetCohortsQueryVariables>(GetCohortsDocument, options);
+        }
+export type GetCohortsQueryHookResult = ReturnType<typeof useGetCohortsQuery>;
+export type GetCohortsLazyQueryHookResult = ReturnType<typeof useGetCohortsLazyQuery>;
+export type GetCohortsQueryResult = Apollo.QueryResult<GetCohortsQuery, GetCohortsQueryVariables>;
+export type GetCohortsQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type GetCohortsQuery = { __typename?: 'Query', cohorts?: { __typename?: 'CohortEntityResponseCollection', data: Array<{ __typename?: 'CohortEntity', attributes?: { __typename?: 'Cohort', name?: string | null, start_date?: any | null, students?: { __typename?: 'StudentRelationResponseCollection', data: Array<{ __typename?: 'StudentEntity', attributes?: { __typename?: 'Student', firstname?: string | null, lastname?: string | null, start_date?: any | null, end_date?: any | null, main_course?: { __typename?: 'CourseEntityResponse', data?: { __typename?: 'CourseEntity', attributes?: { __typename?: 'Course', name?: string | null, achievements?: { __typename?: 'AchievementRelationResponseCollection', data: Array<{ __typename?: 'AchievementEntity', attributes?: { __typename?: 'Achievement', name?: string | null, badge?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', attributes?: { __typename?: 'UploadFile', name: string, alternativeText?: string | null, caption?: string | null } | null } | null } | null } | null }> } | null } | null } | null } | null, avatar?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', attributes?: { __typename?: 'UploadFile', alternativeText?: string | null, name: string, caption?: string | null } | null } | null } | null } | null }> } | null } | null }> } | null };
