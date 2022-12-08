de50836d316dc1c157913e028bc29a95b16295e2
Emily <emilyrachelstickler@gmail.com>
2022-12-08 16:52:08 +0100

    styled component to replace lms css module

--- components/lms-page/LmsContentContainer.tsx
+++ components/lms-page/LmsContentContainer.tsx
@@ -2,6 +2,71 @@ import React, { useEffect } from 'react'
 import Button from '@mui/material/Button'
 import Link from 'next/link'
 import styles from '../../styles/LmsContentContainer.module.css';
+import { styled } from '@mui/material/styles'
+
+export const LmsContent = styled('div')`
+  max-width: 100%;
+  word-wrap: break-word;
+  display: flex;
+  flex-direction: column;
+
+  ${'code'} {
+    background: ${({ theme }) => theme.palette.background.paper};
+    border-left: 3px solid ${({ theme }) => theme.palette.primary.main};
+    border-radius: 0.5em;
+    color: ${({ theme }) => theme.palette.text.primary};
+    font-family: monospace;
+    font-size: 15px;
+    line-height: 1.6;
+    margin: 1em 0;
+    max-width: 100%;
+    overflow: auto;
+    padding: 1em 1.5em;
+    display: block;
+  }
+
+  ${'code'}::before, ${'code'}::after {
+    display: none;
+  }
+
+  .copyButton {
+    position: absolute;
+    background-color: transparent;
+    color: ${({ theme }) => theme.palette.text.primary};
+    right: 4em;
+    margin-top: -0.5em;
+    margin-right: -0.5em;
+    padding: 0.5em;
+    opacity: 0.3;
+    border: solid 1px ${({ theme }) => theme.palette.divider};
+    border-radius: 8px;
+    transition: 0.2s;
+  }
+
+  .copyButton:active {
+    opacity: 1;
+    transition: 0.2s;
+    background-color: ${({ theme }) => theme.palette.primary.light};
+  }
+
+  ${'p'}:first-child img {
+    max-width: 100%;
+    text-align: center;
+  }
+
+  ${'a'} {
+    color: ${({ theme }) => theme.palette.primary.light};
+  }
+
+  @media (hover:hover) {
+    .copyButton:hover {
+      opacity: 1;
+      transition: 0.2s;
+      cursor: pointer;
+      background-color: ${({ theme }) => theme.palette.action.hover}
+    }
+  }
+`
 
 function LmsContentContainer({ content, next, prev }: { content: string, next?: string, prev?: string }) {
 
@@ -9,7 +74,7 @@ function LmsContentContainer({ content, next, prev }: { content: string, next?: 
     const codeTags = document.getElementsByTagName("code");
     for (let i = 0; i < codeTags.length; i++) {
       const copyButton = document.createElement("button");
-      copyButton.classList.add(styles.copyButton);
+      copyButton.classList.add("copyButton");
       copyButton.innerHTML = "Copy";
       copyButton.setAttribute('title', "Copy snippet");
       copyButton.addEventListener("click", () => copyToClipboard(codeTags[i].lastChild?.textContent));
@@ -33,7 +98,7 @@ function LmsContentContainer({ content, next, prev }: { content: string, next?: 
 
   return (
     <>
-      <div className={styles.content} dangerouslySetInnerHTML={{ __html: content }} />
+      <LmsContent dangerouslySetInnerHTML={{ __html: content }} />
 
       <div style={{ padding: 5, width: "100%", display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
         {prev &&
@@ -47,7 +112,6 @@ function LmsContentContainer({ content, next, prev }: { content: string, next?: 
           </Button>
         }
       </div>
-      {/* {addCopyButtons()} */}
     </>
 
   )
