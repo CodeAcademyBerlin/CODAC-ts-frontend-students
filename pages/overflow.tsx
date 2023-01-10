6a2a3f01261512131ccb46b22459c08e0dc3fe1b
Sarah Manicke <sm.wit2022@startsteps.de>
2023-01-10 14:03:57 +0100

    Created a box and h1 title

--- pages/overflow.tsx
+++ pages/overflow.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
+//import auth to get the actual user information
+import { useAuth } from '../hooks/useAuth';
+
+// ** MUI Imports
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 type Props = {}
 
 const Overflow = (props: Props) => {
+  const { user } = useAuth();
+  console.log('user', user);
   return (
-    <div>Overflow</div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > :not(style)': {
+          m: 1,
+          width: "100%",
+          height: "auto",
+        },
+      }}
+    >
+      <Paper
+        elevation={0}
+        style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        alignItems: 'center'
+        }}
+      >
+        <h1>CODAC Overflow</h1>
+
+      </Paper>
+    </Box>
   )
 }
 
