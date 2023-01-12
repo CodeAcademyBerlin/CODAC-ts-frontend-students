c039e2b8122c07caf7e7ea3467cf34fbae0c6457
Sarah Manicke <sm.wit2022@startsteps.de>
2023-01-12 12:08:52 +0100

    sytled index.tsx and get data on dynamic route

--- pages/overflow/[overflowId].tsx
+++ pages/overflow/[overflowId].tsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+//import serverSideProps
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+//import Apollo für ServerSideProps
+import { initializeApollo } from "../../lib/apolloClient";
+//import generated query
+import { GetCodacOverflowsDocument } from "../../cabServer/queries/__generated__/overflow";
 
-type Props = {}
+// ** MUI Imports
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
-const OverflowTopic = (props: Props) => {
+const OverflowTopic = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+
+  const [result, setResult] = useState(data.codacOverflows.data);
+  console.log('result data', result);
+
   return (
-    <div>OverflowTopic</div>
+    <div>
+      <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > :not(style)': {
+          m: 1,
+          width: "100%",
+          height: "auto",
+        },
+      }}
+    >
+      <Paper
+        elevation={0}
+        style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        alignItems: 'center'
+        }}
+      >
+        Overflow-Topic
+      </Paper>
+      </Box>
+    </div>
   )
 }
 
-export default OverflowTopic
+export default OverflowTopic;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const client = initializeApollo(null, ctx.req);
+    const { data, error } = await client.query({
+      query: GetCodacOverflowsDocument
+    });
+    console.log("Fetch was successful, see success:", data);
+    return {
+      props: { data },
+    };
+  } catch (error) {
+    console.log("Fetch was not successful, see error:", error);
+    return {
+      props: { data: null },
+    };
+  }
+};
--- pages/overflow/index.tsx
+++ pages/overflow/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { Key, useState } from 'react';
 //import auth to get the actual user information
 import { useAuth } from '../../hooks/useAuth';
-//import servSideProps
+//import serverSideProps
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 //import types
 import { CodacOverflowEntity } from "../../cabServer/global/__generated__/types";
@@ -9,49 +9,15 @@ import { CodacOverflowEntity } from "../../cabServer/global/__generated__/types"
 import { initializeApollo } from "../../lib/apolloClient";
 //import generated query
 import { GetCodacOverflowsDocument } from "../../cabServer/queries/__generated__/overflow";
+//import for the Styled Link, which not refreshes
+import StyledLink from '../../components/common/StyledLink';
 
 // ** MUI Imports
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import InputBase from '@mui/material/InputBase';
-import { styled, alpha } from '@mui/material/styles';
-
-// ** Icons Imports
-import AccountOutline from "mdi-material-ui/AccountOutline";
-
-//Search declaration from MUI
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 
 //Paper Styling from MUI
 const Item = styled(Paper)(({ theme }) => ({
@@ -66,10 +32,17 @@ const Item = styled(Paper)(({ theme }) => ({
 const Overflow = ({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  /* const [result, setResult] = useState(data.codacOverflows.data); */
+  const [result, setResult] = useState(data.codacOverflows.data);
   const { user } = useAuth();
+  const [searchedTopic, setSearchedTopic] = useState("");
   console.log('user', user);
-  console.log('data', data);
+  console.log('result data', result);
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('searchedTopic', searchedTopic);
+  };
+
   return (
     <Box
       sx={{
@@ -97,7 +70,7 @@ const Overflow = ({
 
         <p>Welcome to CODAC Overflow</p>
 
-        <p>Here you can ask questions, if you get stuck in your current project.<br></br>
+        <p style={{ textAlign: "center" }}>Here you can ask questions, if you get stuck in your current project.<br></br>
           The other students will try their best to help you out.<br></br>
           It is a give and get basically.</p>
 
@@ -113,24 +86,43 @@ const Overflow = ({
         alignItems: 'center'
         }}
       >
-        <Box sx={{
-          borderBottom: (theme) => `1px solid ${theme.palette.divider}`,
-          width: "100%"
-        }}>
-        <Search>
-          <StyledInputBase
-            style={{ width: "100%", textAlign: 'center'}}
-            placeholder="Search…"
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </Search>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 3, width: '100%' },
+            borderBottom: (theme) => `1px solid ${theme.palette.divider}`,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <form onSubmit={handleSearch}>
+            <div>
+              <TextField id="outlined-search" value={searchedTopic} label="Search for topics" name="search" type="search" onChange={(e) => {setSearchedTopic(e.target.value)} }/>
+            </div>
+          </form>
         </Box>
 
-        <Stack spacing={2}>
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
-      </Stack>
+        <Stack spacing={2} style={{ margin: "10px 0px 10px 0px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          {result && result.map((topic: CodacOverflowEntity) => (
+            <Item
+              key={topic.id}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                width: "70%",
+                maxWidth: "70%",
+              }}>
+              <StyledLink href={`overflow/${topic.id}`}><h3 id="dynamic-overflow-link">{topic.attributes?.title}</h3></StyledLink>
+              {topic.attributes?.description}
+            </Item>
+          ))}
+        </Stack>
       </Paper>
     </Box>
   )
