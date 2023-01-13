4bcf762f7919b802ef68b20395ba7878fb16941a
agnejan <agne.jan@hotmail.com>
2023-01-13 12:23:21 +0100

    refactor: removed comments

--- src/pages/battles.tsx
+++ src/pages/battles.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import BattleCard from '../components/battles-page/BattleCard';
 import {
   VsBattle,
   VsBattleEntity,
 } from '../../cabServer/global/__generated__/types';
 import { useGetVsBattlesQuery } from '../../cabServer/queries/__generated__/battles';
-import BattleCard from '../components/battles-page/BattleCard';
+import { GetServerSideProps } from 'next/types';
+import { initializeApollo } from '../lib/apolloClient';
+import {
+  useVoteVsBattleMutation,
+  VoteVsBattleDocument,
+} from '../../cabServer/mutations/__generated__/battles';
+
 type Props = {};
 
 type VsBattles = VsBattle[];
 
 function Battle({}: Props) {
-  const { data, loading, error } = useGetVsBattlesQuery();
+  const { data, loading, error, refetch } = useGetVsBattlesQuery();
+  const [voteVsBattleMutation, { data: mutationData, error: mutationError }] =
+    useVoteVsBattleMutation();
+
+  const handleVote = (vsBattleId: string, option: number) => {
+    voteVsBattleMutation({
+      variables: {
+        vsBattleId: vsBattleId,
+        option: option,
+      },
+    });
+  };
+
+  useEffect(() => {
+    refetch();
+  }, [mutationData]);
+
   const vsBattles = data?.vsBattles?.data || [];
 
   return (
     <div>
       {vsBattles &&
-        vsBattles.map((battle) => {
+        vsBattles.map((battle, index) => {
           if (battle.attributes) {
             return (
-              <>
-                <BattleCard vsBattle={battle.attributes} />
-              </>
+              <div key={index}>
+                <BattleCard vsBattle={battle} handleVote={handleVote} />
+              </div>
             );
           }
         })}
