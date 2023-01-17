313d93dbf0a36009d19514284e068e09ab9f8fe9
AntjeInTech2022 <as.wit2022@startsteps.de>
2023-01-17 10:54:56 +0100

    fixed display logic of comments

--- src/components/lms-page/lmsComments.tsx
+++ src/components/lms-page/lmsComments.tsx
@@ -19,8 +19,7 @@ const LmsComments = ({ slug }: LMSfeedbackProps) => {
   const lmsComments = data?.lmsFeedbacks?.data || [];
   console.log('comments', lmsComments);
 
-  return (
-    lmsComments ? (
+  return lmsComments ? (
     <>
       <Box
         sx={{
@@ -32,48 +31,51 @@ const LmsComments = ({ slug }: LMSfeedbackProps) => {
         <Typography variant="h5">Comments</Typography>
       </Box>
 
-        {lmsComments &&
-          lmsComments.map((lmsComment) => (
-            <Box
-              sx={{
-                p: 5,
-                // display: 'flex',
-                width: '50%',
-              }}
-              key={lmsComment.id}
-            >
-              <Paper style={{ padding: '40px 20px' }}>
-                <Grid container wrap="nowrap" spacing={2}>
-                  <Grid item>
-                    <Avatar
-                    // alt={username}
-                    // src={imgLink}
-                    />
-                  </Grid>
-                  <Grid justifyContent="left" item xs zeroMinWidth>
-                    <h4 style={{ margin: 0, textAlign: 'left' }}>
-                      {lmsComment.attributes?.comments?.map(
-                        (comment) => comment?.author?.data?.attributes?.username,
-                      )}
-                    </h4>
-                    <p style={{ textAlign: 'left' }}>
-                      {lmsComment.attributes?.comments?.map(
-                        (comment) => comment?.message,
-                      )}
-                    </p>
-                    <p style={{ textAlign: 'left', color: 'gray' }}>
-                      {lmsComment.attributes?.comments?.map((comment) =>
-                        new Date(comment?.timestamp).toUTCString(),
-                      )}
-                    </p>
-                  </Grid>
+      {lmsComments &&
+        lmsComments.map((lmsComment) => (
+          <Box
+            sx={{
+              p: 5,
+              // display: 'flex',
+              width: '50%',
+            }}
+            key={lmsComment.id}
+          >
+            <Paper style={{ padding: '40px 20px' }}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid item>
+                  <Avatar
+                  // alt={username}
+                  // src={imgLink}
+                  />
                 </Grid>
-              </Paper>
-            </Box>}
-      <Divider variant="fullWidth" style={{ margin: '30px 0' }} />  
-    </>)) : (
-        <p>no comments yet</p>)
+                <Grid justifyContent="left" item xs zeroMinWidth>
+                  <h4 style={{ margin: 0, textAlign: 'left' }}>
+                    {lmsComment.attributes?.comments?.map(
+                      (comment) => comment?.author?.data?.attributes?.username,
+                    )}
+                  </h4>
+                  <p style={{ textAlign: 'left' }}>
+                    {lmsComment.attributes?.comments?.map(
+                      (comment) => comment?.message,
+                    )}
+                  </p>
+                  <p style={{ textAlign: 'left', color: 'gray' }}>
+                    {lmsComment.attributes?.comments?.map((comment) =>
+                      new Date(comment?.timestamp).toUTCString(),
+                    )}
+                  </p>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Box>
+        ))}
 
+      <Divider variant="fullWidth" style={{ margin: '30px 0' }} />
+    </>
+  ) : (
+    <p>no comments yet. be the first to comment</p>
+  );
 };
 
 export default LmsComments;
