571ca8b828bc2000577ef9807e42b5ecdf020ac4
katia_bezzina <katia.bezzina@gmail.com>
2023-01-23 16:32:09 +0100

    feat: upload file reusable coponent

--- src/components/common/UploadFile.tsx
+++ src/components/common/UploadFile.tsx
@@ -1,48 +1,77 @@
-import { gql, useMutation } from '@apollo/client';
-import styled from '@emotion/styled';
-import { Box, Button, Typography } from '@mui/material';
-import { ChangeEvent, useState } from 'react';
-
-//Upload file graphql mutation
-const MUTATION = gql`
-  mutation SingleUpload($file: Upload!) {
-    singleUpload(file: $file) {
-      id
-      path
-      filename
-      mimetype
-    }
-  }
-`;
+import { styled, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button, { ButtonProps } from '@mui/material/Button';
+import React, { ElementType, useState } from 'react';
+import { getToken } from 'src/lib/apolloClient';
 
 //Upload Image Style
-const ImgStyled = styled('img')(() => ({
+const ImgStyled = styled('img')(({ theme }) => ({
   width: 120,
   height: 120,
-  marginRight: 6.25,
-  borderRadius: 10,
+  marginRight: theme.spacing(6.25),
+  borderRadius: theme.shape.borderRadius,
 }));
 
-const UploadFile = () => {
-  const [image, setImage] = useState<string>('');
+const ButtonStyled = styled(Button)<
+  ButtonProps & { component?: ElementType; htmlFor?: string }
+>(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    textAlign: 'center',
+  },
+}));
 
-  const [mutate] = useMutation(MUTATION);
+const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
+  marginLeft: theme.spacing(4.5),
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    marginLeft: 0,
+    textAlign: 'center',
+    marginTop: theme.spacing(4),
+  },
+}));
 
-  function onChange(target: ChangeEvent) {
-    target: {
-      validity;
-      file: [];
+interface UploadFileProps {
+  setFileId: (fileId: string) => void;
+}
+
+const UploadFile = ({ setFileId }: UploadFileProps) => {
+  const [uploadedFile, setUploadedFile] = useState<string>('');
+
+  async function onChange(e: any) {
+    const token = getToken();
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', `Bearer ${token}`);
+    const file = await e.target.files[0];
+    const form = new FormData();
+    form.append('files', file);
+    console.log('file', file);
+    if (file) {
+      const response = await fetch(
+        'https://codac-364707.ey.r.appspot.com/api/upload',
+        {
+          method: 'post',
+          body: form,
+          headers: myHeaders,
+        },
+      );
+      console.log('response', response);
+      const result = await response.json();
+      setFileId(result[0].id);
+      console.log('result', result);
+      setUploadedFile(result[0].url);
     }
-  }
-  {
-    if (validity.valid) mutate({ variables: { file } });
   }
 
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <ImgStyled src={image} alt="news post cover image" />
+      <ImgStyled src={uploadedFile} alt="news post cover image" />
       <Box>
-        <Button sx={{ mt: 3, ml: 1 }} type="submit" variant="contained">
+        <ButtonStyled
+          component="label"
+          variant="contained"
+          htmlFor="account-settings-upload-image"
+        >
           Upload image
           <input
             hidden
@@ -51,14 +80,14 @@ const UploadFile = () => {
             accept="image/png, image/jpeg"
             id="account-settings-upload-image"
           />
-        </Button>
-        <Button
-          color="secondary"
+        </ButtonStyled>
+        <ResetButtonStyled
+          color="error"
           variant="outlined"
-          onClick={() => setImage('')}
+          onClick={() => setUploadedFile('')}
         >
           Reset
-        </Button>
+        </ResetButtonStyled>
         <Typography variant="body2" sx={{ marginTop: 5 }}>
           Allowed PNG or JPEG. Max size of 800K.
         </Typography>
