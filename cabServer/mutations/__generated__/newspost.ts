c0517cbbd838f2c820c846cd18f95541c6a90419
katia_bezzina <katia.bezzina@gmail.com>
2023-01-26 11:34:50 +0100

    news related query, mutation, types

--- cabServer/mutations/__generated__/newspost.ts
+++ cabServer/mutations/__generated__/newspost.ts
@@ -1,77 +1,60 @@
+import * as Types from '../../global/__generated__/types';
+
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
-
-import * as Types from '../../global/__generated__/types';
 const defaultOptions = {} as const;
 
 export const CreateNewsPostDocument = gql`
-  mutation createNewsPost(
-    $title: String!
-    $post: String!
-    $author: ID!
-    $tags: ENUM_NEWSPOST_TAGS!
-    $image: ID!
-    $publishedAt: DateTime!
+    mutation createNewsPost($title: String!, $post: String!, $author: ID!, $tags: ENUM_NEWSPOST_TAGS!, $image: ID!, $publishedAt: DateTime!) {
+  createNewsPost(
+    data: {title: $title, post: $post, image: $image, author: $author, tags: $tags, publishedAt: $publishedAt}
   ) {
-    createNewsPost(
-      data: {
-        title: $title
-        post: $post
-        image: $image
-        author: $author
-        tags: $tags
-        publishedAt: $publishedAt
-      }
-    ) {
-      data {
-        id
-        attributes {
-          title
-          post
-          author {
-            data {
-              id
-              attributes {
-                username
-                avatar {
-                  data {
-                    id
-                    attributes {
-                      name
-                      alternativeText
-                      width
-                      height
-                      hash
-                      mime
-                      size
-                      previewUrl
-                      provider
-                      url
-                    }
+    data {
+      id
+      attributes {
+        title
+        post
+        author {
+          data {
+            id
+            attributes {
+              username
+              avatar {
+                data {
+                  id
+                  attributes {
+                    name
+                    alternativeText
+                    width
+                    height
+                    hash
+                    mime
+                    size
+                    previewUrl
+                    provider
+                    url
                   }
                 }
               }
             }
           }
-          tags
-          publishedAt
-          image {
-            data {
-              id
-              attributes {
-                url
-              }
+        }
+        tags
+        publishedAt
+        image {
+          data {
+            id
+            attributes {
+              url
             }
           }
         }
       }
     }
   }
-`;
-export type CreateNewsPostMutationFn = Apollo.MutationFunction<
-  CreateNewsPostMutation,
-  CreateNewsPostMutationVariables
->;
+}
+    `;
+export type CreateNewsPostMutationFn = Apollo.MutationFunction<CreateNewsPostMutation, CreateNewsPostMutationVariables>;
 
 /**
  * __useCreateNewsPostMutation__
@@ -95,27 +78,13 @@ export type CreateNewsPostMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useCreateNewsPostMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    CreateNewsPostMutation,
-    CreateNewsPostMutationVariables
-  >,
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useMutation<
-    CreateNewsPostMutation,
-    CreateNewsPostMutationVariables
-  >(CreateNewsPostDocument, options);
-}
-export type CreateNewsPostMutationHookResult = ReturnType<
-  typeof useCreateNewsPostMutation
->;
-export type CreateNewsPostMutationResult =
-  Apollo.MutationResult<CreateNewsPostMutation>;
-export type CreateNewsPostMutationOptions = Apollo.BaseMutationOptions<
-  CreateNewsPostMutation,
-  CreateNewsPostMutationVariables
->;
+export function useCreateNewsPostMutation(baseOptions?: Apollo.MutationHookOptions<CreateNewsPostMutation, CreateNewsPostMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateNewsPostMutation, CreateNewsPostMutationVariables>(CreateNewsPostDocument, options);
+      }
+export type CreateNewsPostMutationHookResult = ReturnType<typeof useCreateNewsPostMutation>;
+export type CreateNewsPostMutationResult = Apollo.MutationResult<CreateNewsPostMutation>;
+export type CreateNewsPostMutationOptions = Apollo.BaseMutationOptions<CreateNewsPostMutation, CreateNewsPostMutationVariables>;
 export type CreateNewsPostMutationVariables = Types.Exact<{
   title: Types.Scalars['String'];
   post: Types.Scalars['String'];
@@ -125,59 +94,5 @@ export type CreateNewsPostMutationVariables = Types.Exact<{
   publishedAt: Types.Scalars['DateTime'];
 }>;
 
-export type CreateNewsPostMutation = {
-  __typename?: 'Mutation';
-  createNewsPost?: {
-    __typename?: 'NewsPostEntityResponse';
-    data?: {
-      __typename?: 'NewsPostEntity';
-      id?: string | null;
-      attributes?: {
-        __typename?: 'NewsPost';
-        title?: string | null;
-        post?: string | null;
-        tags?: Types.Enum_Newspost_Tags | null;
-        publishedAt?: any | null;
-        author?: {
-          __typename?: 'UsersPermissionsUserEntityResponse';
-          data?: {
-            __typename?: 'UsersPermissionsUserEntity';
-            id?: string | null;
-            attributes?: {
-              __typename?: 'UsersPermissionsUser';
-              username: string;
-              avatar?: {
-                __typename?: 'UploadFileEntityResponse';
-                data?: {
-                  __typename?: 'UploadFileEntity';
-                  id?: string | null;
-                  attributes?: {
-                    __typename?: 'UploadFile';
-                    name: string;
-                    alternativeText?: string | null;
-                    width?: number | null;
-                    height?: number | null;
-                    hash: string;
-                    mime: string;
-                    size: number;
-                    previewUrl?: string | null;
-                    provider: string;
-                    url: string;
-                  } | null;
-                } | null;
-              } | null;
-            } | null;
-          } | null;
-        } | null;
-        image?: {
-          __typename?: 'UploadFileEntityResponse';
-          data?: {
-            __typename?: 'UploadFileEntity';
-            id?: string | null;
-            attributes?: { __typename?: 'UploadFile'; url: string } | null;
-          } | null;
-        } | null;
-      } | null;
-    } | null;
-  } | null;
-};
+
+export type CreateNewsPostMutation = { __typename?: 'Mutation', createNewsPost?: { __typename?: 'NewsPostEntityResponse', data?: { __typename?: 'NewsPostEntity', id?: string | null, attributes?: { __typename?: 'NewsPost', title?: string | null, post?: string | null, tags?: Types.Enum_Newspost_Tags | null, publishedAt?: any | null, author?: { __typename?: 'UsersPermissionsUserEntityResponse', data?: { __typename?: 'UsersPermissionsUserEntity', id?: string | null, attributes?: { __typename?: 'UsersPermissionsUser', username: string, avatar?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', id?: string | null, attributes?: { __typename?: 'UploadFile', name: string, alternativeText?: string | null, width?: number | null, height?: number | null, hash: string, mime: string, size: number, previewUrl?: string | null, provider: string, url: string } | null } | null } | null } | null } | null } | null, image?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', id?: string | null, attributes?: { __typename?: 'UploadFile', url: string } | null } | null } | null } | null } | null } | null };
--- cabServer/queries/__generated__/news.ts
+++ cabServer/queries/__generated__/news.ts
@@ -1,48 +1,67 @@
+import * as Types from '../../global/__generated__/types';
+
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
-
-import * as Types from '../../global/__generated__/types';
 const defaultOptions = {} as const;
 
 export const GetNewsDocument = gql`
-  query getNews {
-    newsPosts(sort: "updatedAt:desc") {
-      data {
-        attributes {
-          title
-          post
-          author {
-            data {
-              id
-              attributes {
-                username
+    query getNews {
+  newsPosts(sort: "updatedAt:desc") {
+    data {
+      id
+      attributes {
+        title
+        post
+        author {
+          data {
+            id
+            attributes {
+              username
+              avatar {
+                data {
+                  id
+                  attributes {
+                    name
+                    alternativeText
+                    width
+                    height
+                    hash
+                    mime
+                    size
+                    previewUrl
+                    provider
+                    url
+                  }
+                }
               }
             }
           }
-          likes {
-            data {
-              id
-              attributes {
-                username
-              }
+        }
+        likes {
+          data {
+            id
+            attributes {
+              username
             }
           }
-          createdAt
-          updatedAt
-          publishedAt
-          tags
-          image {
-            data {
-              attributes {
-                url
-              }
+        }
+        createdAt
+        updatedAt
+        publishedAt
+        tags
+        image {
+          data {
+            id
+            attributes {
+              url
             }
           }
         }
       }
     }
   }
-`;
+}
+    `;
 
 /**
  * __useGetNewsQuery__
@@ -59,79 +78,18 @@ export const GetNewsDocument = gql`
  *   },
  * });
  */
-export function useGetNewsQuery(
-  baseOptions?: Apollo.QueryHookOptions<GetNewsQuery, GetNewsQueryVariables>,
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<GetNewsQuery, GetNewsQueryVariables>(
-    GetNewsDocument,
-    options,
-  );
-}
-export function useGetNewsLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<
-    GetNewsQuery,
-    GetNewsQueryVariables
-  >,
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<GetNewsQuery, GetNewsQueryVariables>(
-    GetNewsDocument,
-    options,
-  );
-}
+export function useGetNewsQuery(baseOptions?: Apollo.QueryHookOptions<GetNewsQuery, GetNewsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetNewsQuery, GetNewsQueryVariables>(GetNewsDocument, options);
+      }
+export function useGetNewsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetNewsQuery, GetNewsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetNewsQuery, GetNewsQueryVariables>(GetNewsDocument, options);
+        }
 export type GetNewsQueryHookResult = ReturnType<typeof useGetNewsQuery>;
 export type GetNewsLazyQueryHookResult = ReturnType<typeof useGetNewsLazyQuery>;
-export type GetNewsQueryResult = Apollo.QueryResult<
-  GetNewsQuery,
-  GetNewsQueryVariables
->;
-export type GetNewsQueryVariables = Types.Exact<{ [key: string]: never }>;
+export type GetNewsQueryResult = Apollo.QueryResult<GetNewsQuery, GetNewsQueryVariables>;
+export type GetNewsQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
-export type GetNewsQuery = {
-  __typename?: 'Query';
-  newsPosts?: {
-    __typename?: 'NewsPostEntityResponseCollection';
-    data: Array<{
-      __typename?: 'NewsPostEntity';
-      attributes?: {
-        __typename?: 'NewsPost';
-        title?: string | null;
-        post?: string | null;
-        createdAt?: any | null;
-        updatedAt?: any | null;
-        publishedAt?: any | null;
-        tags?: Types.Enum_Newspost_Tags | null;
-        author?: {
-          __typename?: 'UsersPermissionsUserEntityResponse';
-          data?: {
-            __typename?: 'UsersPermissionsUserEntity';
-            id?: string | null;
-            attributes?: {
-              __typename?: 'UsersPermissionsUser';
-              username: string;
-            } | null;
-          } | null;
-        } | null;
-        likes?: {
-          __typename?: 'UsersPermissionsUserRelationResponseCollection';
-          data: Array<{
-            __typename?: 'UsersPermissionsUserEntity';
-            id?: string | null;
-            attributes?: {
-              __typename?: 'UsersPermissionsUser';
-              username: string;
-            } | null;
-          }>;
-        } | null;
-        image?: {
-          __typename?: 'UploadFileEntityResponse';
-          data?: {
-            __typename?: 'UploadFileEntity';
-            attributes?: { __typename?: 'UploadFile'; url: string } | null;
-          } | null;
-        } | null;
-      } | null;
-    }>;
-  } | null;
-};
+
+export type GetNewsQuery = { __typename?: 'Query', newsPosts?: { __typename?: 'NewsPostEntityResponseCollection', data: Array<{ __typename?: 'NewsPostEntity', id?: string | null, attributes?: { __typename?: 'NewsPost', title?: string | null, post?: string | null, createdAt?: any | null, updatedAt?: any | null, publishedAt?: any | null, tags?: Types.Enum_Newspost_Tags | null, author?: { __typename?: 'UsersPermissionsUserEntityResponse', data?: { __typename?: 'UsersPermissionsUserEntity', id?: string | null, attributes?: { __typename?: 'UsersPermissionsUser', username: string, avatar?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', id?: string | null, attributes?: { __typename?: 'UploadFile', name: string, alternativeText?: string | null, width?: number | null, height?: number | null, hash: string, mime: string, size: number, previewUrl?: string | null, provider: string, url: string } | null } | null } | null } | null } | null } | null, likes?: { __typename?: 'UsersPermissionsUserRelationResponseCollection', data: Array<{ __typename?: 'UsersPermissionsUserEntity', id?: string | null, attributes?: { __typename?: 'UsersPermissionsUser', username: string } | null }> } | null, image?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', id?: string | null, attributes?: { __typename?: 'UploadFile', url: string } | null } | null } | null } | null }> } | null };
