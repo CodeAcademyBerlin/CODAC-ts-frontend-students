534ef2db68ac3b4fae4c921f00b14eafc9822d2c
Viktor Tagil <viktor.tagil@gmail.com>
2023-02-14 19:16:12 +0100

    Challenge component and test cleanup

--- src/components/coding-challenges-page/ChallengeCard.test.tsx
+++ src/components/coding-challenges-page/ChallengeCard.test.tsx
@@ -1,9 +1,10 @@
 import { getByText, render, screen } from '@testing-library/react';
 import { CodingChallengeEntity } from 'cabServer/global/__generated__/types';
+import TestRenderer from 'react-test-renderer';
 
 import ChallengeCard from '../../components/coding-challenges-page/ChallengeCard';
 
-// use entity to make sure its the right shape
+// Use CodingChallengeEntity to make sure we build the mock data in the correct format
 
 describe('Codingchallenge card content', () => {
   test('renders content', () => {
@@ -11,17 +12,25 @@ describe('Codingchallenge card content', () => {
       attributes: {
         title: 'test',
         challenge: 'test body',
-        // random: 'hi',
       },
     };
 
     render(<ChallengeCard challenge={challenge} />);
-    const challengeCardElement = screen.getByTestId('example');
-    expect(challengeCardElement).toBeInTheDocument();
-    expect(challengeCardElement).toHaveTextContent('See challenge');
 
-    // other
+    const challengeCardElement = screen.getByText('See challenge');
+
+    expect(challengeCardElement).toHaveTextContent('See challenge');
+    // expect(challengeCardElement).toBeInTheDocument();
+
     const title = screen.getByText(challenge?.attributes?.title!);
+    const challengeElement = screen.getByText(
+      challenge?.attributes?.challenge!,
+    );
+
     expect(title).toHaveTextContent(challenge?.attributes?.title!);
+    expect(challengeElement).toHaveTextContent(
+      challenge?.attributes?.challenge!,
+    );
   });
+  test.todo('Check that contributor is rendered');
 });
--- src/components/coding-challenges-page/ChallengeCard.tsx
+++ src/components/coding-challenges-page/ChallengeCard.tsx
@@ -10,34 +10,23 @@ import { CodingChallengeEntity } from 'cabServer/global/__generated__/types';
 import React from 'react';
 
 import StyledLink from '../common/StyledLink';
-// describes the object that it will recive
+
+// The type to describe the object I will receive in this component. Want to recive the full CodingChallengeEntity. In jobcardtest we use this to enforce data structure
 
 type ChallengeProps = {
   challenge: CodingChallengeEntity;
-  // title: string
 };
+// If you have several props its better to creare a type otherwise alt way would be to include like so: const ChallengeCard = ({ challenge }: { challenge: CodingChallengeEntity })
 
 const ChallengeCard = ({ challenge }: ChallengeProps) => {
-  // console.log('props.challenge', props?.challenge);
-  console.log('challenge.id', challenge.id);
-  const { id } = challenge;
-  console.log('id', id);
-
-  console.log('challenge', challenge);
-  console.log('challenge.attributes', challenge.attributes);
-
-  const { attributes } = challenge;
-  console.log('attributes', attributes);
-
+  const { id, attributes } = challenge;
   const title = challenge?.attributes?.title || 'no title';
-  console.log('title', title);
 
-  // option 1 leave and then when you render
-  // option 2 || when no title , "no title"
+  // Two ways of dealing with possiblity that title (ot other data that is not required being null): 1) handle in render 2) avoid destructuring and handle when creating variable with ||
 
   return (
     <>
-      <div data-testid="example">
+      <div>
         <Card sx={{ position: 'relative' }}>
           <CardContent>
             <Box
@@ -45,28 +34,26 @@ const ChallengeCard = ({ challenge }: ChallengeProps) => {
                 mt: 5.75,
                 mb: 8.75,
                 display: 'flex',
+                direction: 'column',
                 flexWrap: 'wrap',
                 alignItems: 'center',
-                justifyContent: 'space-between',
+                justifyContent: 'space-around',
               }}
             >
               <Box
                 sx={{ mr: 2, mb: 1, display: 'flex', flexDirection: 'column' }}
               >
-                <Typography variant="h6">
-                  {challenge?.attributes?.title}
+                <Typography variant="h6">{title}</Typography>
+                <Typography variant="caption">
+                  {attributes?.challenge}
                 </Typography>
                 <Typography variant="caption">
-                  {challenge?.attributes?.challenge}
+                  To be added: contributor
                 </Typography>
-                <Typography variant="caption">TBA contributor</Typography>
               </Box>
               <StyledLink href={`/codingchallenges/${challenge?.id}`}>
                 <Button variant="contained">See challenge</Button>
               </StyledLink>
-              {/* <StyledLink href={`/codingchallenges/${props?.challenge?.id}`}>
-                <Button variant="contained">See challenge</Button>
-              </StyledLink> */}
             </Box>
           </CardContent>
         </Card>
