4c50d62728e0263232b074107622d949555fe0bd
JacoLombardo <jacopo.lombardo@outlook.com>
2023-02-16 16:35:07 +0100

    build: :bug: 1

--- src/pages/api/lms-search.ts
+++ src/pages/api/lms-search.ts
@@ -12,22 +12,3 @@ export default async function API(req: any, res: any) {
     console.log('error', error);
   };
 }
-
-// export default async function searchInput(req: any, res: any) {
-//   const file = path.join(process.cwd(), 'index.json')
-//   const lms = JSON.parse(readFileSync(file, 'utf8'));
-//   const { input } = req.body;
-//   const inputArray = input.split(',');
-//   try {
-//     const filteredIndex: Array<filteredIndex> = [];
-//     for (let i = 0; i < lms.length; i++){
-//       const inputs = filterIndex(lms[i], inputArray);
-//       if (inputs) {
-//         filteredIndex.push(inputs);
-//       };
-//     };
-//     res.status(200).json({ filteredIndex });
-//   } catch (error) {
-//     console.log('error', error);
-//   };
-// }
