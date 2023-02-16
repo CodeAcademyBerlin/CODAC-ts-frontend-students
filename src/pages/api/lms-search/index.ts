7bbc2f5b4beda2677288f59f966a8e888811104f
JacoLombardo <jacopo.lombardo@outlook.com>
2023-02-16 15:30:18 +0100

    build: please

---
+++ src/pages/api/lms-search/index.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { readFileSync } from 'fs';
+import path from 'path';
+import { filteredIndex } from 'src/lib/lms-index';
+import { filterIndex } from 'src/lib/lms-search';
+
+export default async function searchInput(req: any, res: any) {
+  const file = path.join(process.cwd(), 'index.json')
+  const lms = JSON.parse(readFileSync(file, 'utf8'));
+  const { input } = req.body;
+  const inputArray = input.split(',');
+  try {
+    const filteredIndex: Array<filteredIndex> = [];
+    for (let i = 0; i < lms.length; i++){
+      const inputs = filterIndex(lms[i], inputArray);
+      if (inputs) {
+        filteredIndex.push(inputs);
+      };
+    };
+    res.status(200).json({ filteredIndex });
+  } catch (error) {
+    console.log('error', error);
+  };
+}
