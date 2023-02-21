87267ff8284dafe3407fa23cb6e3e4c25357c481
Skycat1983 <hlaoutaris@gmail.com>
2023-02-21 14:19:06 +0100

    strapi config

--- src/components/chat-page/Chat.tsx
+++ src/components/chat-page/Chat.tsx
@@ -9,6 +9,7 @@ import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { Chat } from 'cabServer/global/__generated__/types';
+import { useGetChatsQuery } from 'cabServer/queries/__generated__/chats';
 import Image from 'next/image';
 import React, { useEffect, useRef, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
@@ -17,6 +18,8 @@ import { useAuth } from 'src/hooks/useAuth';
 import { useSocket } from 'src/hooks/useSocket';
 
 import favicon from '../../../public/favicon.ico';
+import ChatHistory from '../chat-room/ChatHistory';
+import { ChatBubble } from './ChatBubble';
 
 //! logged ibnuser
 
@@ -35,276 +38,194 @@ interface IMsg {
   ts: string;
   // rn: number;
 }
-//? useAuth userobject
-// component
+
 const ChatRoom: React.FC = () => {
-  // const Chat: React.FC = ({ socket, roomId }) => {
+  const { data, loading, error } = useGetChatsQuery({
+    variables: {},
+  });
   const { socket } = useSocket();
   const inputRef = useRef(null);
-  // connected flag
   const [connected, setConnected] = useState<boolean>(false);
-
-  // init chat and message
+  const [typing, setTyping] = useState<boolean>(false);
+  const [rooms, setRooms] = useState(null);
   const [chat, setChat] = useState<Chat | null>(null);
+  const [chatHistory, setChatHistory] = useState<Chat[]>([]);
   const [msg, setMsg] = useState<string>('');
   const [ts, setTs] = useState(new Date().toLocaleDateString('de-DE'));
-  const [backlog, setBacklog] = useState([]);
   const { user } = useAuth();
 
   useEffect(() => {
     if (socket) {
+      setConnected(true);
       socket.on('chat:update', (chatEvent: Chat) => {
         setChat(chatEvent);
       });
 
-      socket.on('chat', (chatEvent: Chat) => {
-        console.log(chatEvent);
+      socket.on('newMessage', (chatEvent: Chat) => {
+        console.log('chatEvent in useEffect:', chatEvent);
       });
     }
   }, [socket]);
 
-  const sendMessage = async () => {
-    if (msg) {
-      const message: IMsg = {
-        // user: user.username,
-        user: 'hi',
-        msg,
-        ts,
-        // rn,
-      };
-      // build message obj
-
-      // dispatch message to other users
-      const resp = await fetch(
-        '/api/chat', //? req
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(body),
-        }, //? res
-        // roomId: 'test',
-      );
-      console.warn('resp', resp);
-      // reset field if OK
-      if (resp.ok) setMsg('');
+  useEffect(() => {
+    let roomsArr = [];
+    if (data) {
+      if (user.role?.name === 'Student') {
+        roomsArr = data.chats.data.map((room) => room.attributes.name);
+        console.log('rooms', roomsArr);
+      }
     }
+    setRooms(roomsArr);
+  }, [data]);
+
+  const sendMessage = async () => {
+    // if (msg) {
+    //   const message: IMsg = {
+    //     user: user.username,
+    //     msg,
+    //     ts,
+    //   };
+
+    //   const resp = await fetch(
+    //     '/api/chat', //? req
+    //     {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(body),
+    //     }, //? res
+    //   );
+    //   console.warn('resp', resp);
+    //   if (resp.ok) setMsg('');
+    // }
     console.warn('chat', chat, 'msg', msg);
   };
 
   let userLoggedIn = user;
-  console.warn('user', user);
   const printMessage = (message) => {
     console.warn('message', message);
   };
 
-  const formatDate = (timestamp) => {
-    let formattedDate = timestamp.slice(0, 10);
-    // let formattedDate = timestamp.toLocaleString('en-US', {
-    //   weekday: 'long',
-    //   year: 'numeric',
-    //   month: 'long',
-    //   day: 'numeric',
-    //   hour: 'numeric',
-    //   minute: 'numeric',
-    //   second: 'numeric',
-    //   hour12: true,
-    //   timeZone: 'UTC',
-    // });
+  // console.log(chatHistory);
 
-    return formattedDate;
+  // this function should be called when a room is clicked
+  const joinRoom = (e, t) => {
+    // we use t to get the room name. this is the room name that we want to join
+    socket.emit('chat:join', t);
   };
 
   return (
     <div>
+      <div
+        style={{
+          backgroundColor: 'white',
+          width: '100%',
+          padding: '20px',
+          borderRadius: '10px',
+          // position: 'fixed',
+          zIndex: 1,
+          justifyItems: 'center',
+          position: 'relative',
+        }}
+      >
+        {rooms &&
+          rooms.map((room, i) => (
+            <Button
+              sx={{ m: 1 }}
+              key={i}
+              variant="contained"
+              color="primary"
+              onClick={(e) => joinRoom(e, room)}
+              // onClick={() => {
+              //   // socket.emit('chat:join', room);
+              //   // setChatHistory([...chatHistory, chat]);
+              // }}
+            >
+              {room}
+            </Button>
+          ))}
+        <Button
+          sx={{ m: 1 }}
+          variant="contained"
+          color="primary"
+          onClick={(e) => joinRoom(e, 'general')}
+        >
+          ALL
+        </Button>
+      </div>
+
       {chat && <h1>cohort: {chat.name}</h1>}
-
       {chat &&
-        chat.messages.map((message, i) => (
+        chat.messages?.map((message, i) => (
           <>
-            {printMessage(message)}
-            <div>
-              <h6>author: {message?.authorString}</h6>
-              <h6>{formatDate(message.timestamp)}</h6>
-
-              <p>{message.body}</p>
-            </div>
-            <h1>author: {message?.author.data.attributes.username}</h1>
+            <ChatBubble message={message}></ChatBubble>
           </>
         ))}
+      {typing && (
+        <div
+          style={{
+            position: 'relative',
+            backgroundColor: 'white',
+            // backgroundColor: ' #00897d',
+
+            color: ' #00897d',
+            // color: 'white',
+
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'right',
+            borderRadius: '5px 5px 5px 5px',
+            paddingRight: '10px',
+            // margin: '20px',
+            width: '95%',
+            height: '4vh',
+            textAlign: 'right',
+            left: '1vw',
+            marginBottom: '2vh',
+          }}
+        >
+          <h6 style={{ position: 'absolute', top: '-18px' }}>typing...</h6>
+        </div>
+      )}
+      <div
+        style={{
+          backgroundColor: 'white',
+          width: '95%',
+          height: '110%',
+          padding: '20px',
+          // paddingTop: '20px',
+          borderRadius: '10px',
+        }}
+      >
+        <TextField
+          onFocus={() => setTyping(true)}
+          onBlur={() => setTyping(false)}
+          style={{ width: '90%' }}
+          id="outlined-basic"
+          label={connected ? 'Write something' : 'Connecting...'}
+          variant="standard"
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+          inputRef={inputRef}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              sendMessage();
+            }
+          }}
+        />
+        {/* <Button
+          style={{ width: '80%' }}
+          sx={{ m: 1 }}
+          variant="contained"
+          color="primary"
+          onClick={sendMessage}
+        >
+          Send
+        </Button> */}
+      </div>
     </div>
   );
 };
 
-//! sometimes explosion of errors? server restart?
-
-//!
-//   return (
-//     <div>
-//       <div>
-//         {chat ? (
-//           chat.messages.map((message, i) => (
-//             <div key={'msg_' + i} className="mt-1">
-//               <Box
-//                 sx={{
-//                   overflow: 'scroll',
-//                   height: '100',
-//                   // width: '20',
-//                   my: 1,
-//                   p: 1,
-//                   color: (theme) =>
-//                     theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-//                   borderRadius: 2,
-//                   fontSize: '0.875rem',
-//                   fontWeight: '700',
-//                 }}
-//                 component="div"
-//                 overflow="auto"
-//                 id="scroll"
-//                 flex={1}
-//                 bgcolor="white"
-//               >
-//                 <Item2>
-//                   <Image
-//                     width={15}
-//                     height={15}
-//                     src={favicon}
-//                     alt="Picture of the author"
-//                   ></Image>
-//                   <span>
-//                     {/* {message.author.data.id === user?.id ? 'someone' : chat.user} */}
-//                   </span>
-//                   {message.body}
-//                 </Item2>
-//               </Box>
-//             </div>
-//           ))
-//         ) : (
-//           <Item2>No chat messages</Item2>
-//         )}
-//       </div>
-//       <div
-//         style={{
-//           top: '20vh',
-//           right: '50vw',
-//         }}
-//       >
-//         <ToastContainer
-//           className="toast-position"
-//           position={toast.POSITION.BOTTOM_RIGHT}
-//           newestOnTop={true}
-//         />
-//       </div>
-//       <div
-//         style={{
-//           position: 'fixed',
-//           bottom: '12vh',
-//           right: '27vw',
-//         }}
-//       >
-//         <Stack
-//           style={{
-//             justifyContent: 'center',
-//             bottom: '5vh',
-//           }}
-//           direction={{ xs: 'column', sm: 'row' }}
-//           spacing={{ xs: 1, sm: 1, md: 3 }}
-//         >
-//           <h1
-//             style={{
-//               color: '#444444',
-//             }}
-//           >
-//             Whatsthat
-//           </h1>
-
-//           {/* <QuestionAnswerIcon
-//             style={{
-//               color: ' #00897d',
-//               marginTop: '30',
-//             }}
-//           ></QuestionAnswerIcon> */}
-//           <Button
-//             style={{
-//               display: 'inline-block',
-//               minHeight: 0,
-//               minWidth: 0,
-//               height: 50,
-//               marginTop: 12,
-//               borderRadius: 5,
-//               backgroundColor: ' #ffffff',
-//               color: ' #00897d',
-//               fontSize: '18px',
-//               justifyContent: 'center',
-//             }}
-//             variant="contained"
-//             onClick={sendMessage}
-//             disabled={!connected}
-//           >
-//             SEND
-//           </Button>
-//         </Stack>
-//       </div>
-//       <TextField
-//         fullWidth={true}
-//         color="success"
-//         style={{
-//           width: '25vw',
-//           borderRadius: 10,
-//           borderColor: ' #ffffff',
-//           backgroundColor: ' #ffffff',
-//           position: 'fixed',
-//           right: '25vw',
-//           bottom: '3.5vh',
-
-//           margin: '10px',
-//           fontSize: '18px',
-//         }}
-//         ref={inputRef}
-//         type="text"
-//         value={msg}
-//         placeholder={connected ? 'Write something...' : 'Connecting...'}
-//         disabled={!connected}
-//         onChange={(e) => {
-//           setMsg(e.target.value);
-//         }}
-//         onKeyPress={(e) => {
-//           if (e.key === 'Enter') {
-//             sendMessage();
-//           }
-//         }}
-//       ></TextField>
-//     </div>
-//   );
-// };
-
 export default ChatRoom;
-
-//! QUESTIONS:
-// - see line 98 & chat.ts
-// - _app.tsx page props. where do they come from?
-//! sorting algorithms.
-
-//? dynamic routes to cohorts
-//* tabs for
-
-// serverside cleint side chats.
-
-// chat component. when.
-
-// loigin notifications.
-
-// when establish connection? notifications post login. could logic happen in context?
-//SWR tech. stay while,
-
-// is user ID stored in token? where verified?
-
-// const mentor = 'Mentor_' + String(new Date().getTime()).substr(-3);
-// const user = 'User_' + String(new Date().getTime()).substr(-3);
-
-{
-  /* <Grid style={{ display: "flex", flex: 1 }} container spacing={3}>
-          <Grid style={{ display: "flex", flex: 1 }} item xs={6}>
-            <Box overflow="auto" id="scroll" flex={1} bgcolor="white"></Box> */
-}
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import { useGetChatsQuery } from 'cabServer/queries/__generated__/chats';
+import { useGetCohortsQuery } from 'cabServer/queries/__generated__/cohorts';
 import { forEach } from 'lodash';
 import Image from 'next/image';
 // ** Next Import
@@ -29,28 +30,21 @@ const StyledLink = styled(Link)({
 });
 
 const ChatPage = () => {
-  const { socket, connectSocket } = useSocket();
-  const [test, setTest] = useState();
-  const { data, loading, error } = useGetChatsQuery({
+  const { data, loading, error } = useGetCohortsQuery({
     variables: {},
   });
+  const { socket, connectSocket } = useSocket();
+  const [test, setTest] = useState();
+  // const { data, loading, error } = useGetChatsQuery({
+  //   variables: {},
+  // });
 
   useEffect(() => {
     connectSocket();
+    let cohorts = data?.cohorts.data;
+    console.log(cohorts);
   }, []);
   //! once connection. then chat rooms to choose. connection after choosing
-
-  // useEffect(() => {
-  //   if (socket) {
-  //     socket.on('chat:update', (chat: Chat) => {
-  //       console.log('chat', chat);
-  //       console.warn('data', data);
-  //       setTest(chat);
-  //     });
-  //   }
-  // }, [socket]);
-  // console.warn('data', data);
-  // console.warn('chat', test);
 
   return (
     <Box
@@ -62,10 +56,9 @@ const ChatPage = () => {
         textAlign: 'center',
       }}
     >
-      <Typography variant="h1">Chat</Typography>
+      <Typography variant="h4">Whatsthat</Typography>
       {socket && <Chat />}
       {/* <Typography variant="h6">{data.chats.data}</Typography> */}
-
       {/* {socket && <Chat socket={socket} roomId={'test'} />} */}
     </Box>
   );
@@ -74,7 +67,18 @@ const ChatPage = () => {
 export default ChatPage;
 
 //! Q
-// is users_permissions_user a term you used to refer to the author? or does this not exist yet?
+// i need to get chats based on cohort name
+// it seems the nested user permissions/author relation is not working. it only accesses a count.
+// attempted getCohorts query. didn't work
+// what's this schema for: createChat(data: ChatInput!): ChatEntityResponse
+// create chat message mutation
+// what is variables in useGetChatsQuery?
+// is chatHistory necessary?
+// is role.id a placeholder for cohort id? if not maybe we could add that field?
+// todo: create room options based on permissions of the user
+// is ther best structure for this when we query the chat history for a specific cohort after a 'room' has been chosen, instead of gettiing all chat histories?
+
+//* postChatMessage mutation created
 
 //! graphql mutations
 
