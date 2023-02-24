cdee80bc52aa0674c25a2a417ef09878a02b2b3e
Lucas Dupias <lucas@codeacademyberlin.com>
2023-02-24 15:49:51 +0100

    fix: :label: chat id

--- src/components/chat-page/ChatRooms.tsx
+++ src/components/chat-page/ChatRooms.tsx
@@ -61,7 +61,7 @@ const ChatRooms: React.FC = () => {
             component="span"
             variant="contained"
             color={room.attributes?.name === room ? 'inherit' : 'primary'}
-            onClick={() => setRoom(room?.id)}
+            onClick={() => setRoom(room?.id || '')}
             key={room.id}
           >
             {room.attributes?.name}
